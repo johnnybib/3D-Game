06b9ce82e24a80837a8cffe941f22d12ee223713
Johnnybib <jamesgrahamhu@gmail.com>
2019-01-30 19:57:02 -0700

    Added gravity, added staircase to test scene

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,21 @@ public class PlayerController : MonoBehaviour
     private Rigidbody _rb;
     private Transform _cameraContainer;
     private CharacterController _controller;
+    private Transform _groundChecker;
 
+    private Vector3 _velocity;
+    private bool _isGrounded;
+    private int _ground;
+
+    //[SerializeField]
+    //private float StickThreshold;
     [SerializeField]
-    private float stickThreshold;
+    private float MoveSpeed;//units per second
     [SerializeField]
-    private float moveSpeed;//units per second
+    private float TurnSpeed;//rad per second
     [SerializeField]
-    private float turnSpeed;//rad per second
+    private float GroundDistance;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +29,9 @@ public class PlayerController : MonoBehaviour
         _rb = GetComponent<Rigidbody>();
         _cameraContainer = GameObject.FindWithTag("MainCamera").transform.parent;
         _controller = GetComponent<CharacterController>();
+        _velocity = Vector3.zero;
+        _groundChecker = transform.Find("Ground Checker");
+        _ground = 1 << LayerMask.NameToLayer("Ground");
     }
 
     // Update is called once per frame
@@ -40,15 +51,25 @@ public class PlayerController : MonoBehaviour
 
         Vector3 move = new Vector3(inputX, 0, inputZ);
         Vector3 rotatedMove = _cameraContainer.rotation * move;
-        _controller.Move(rotatedMove * moveSpeed * Time.deltaTime);
+        _controller.Move(rotatedMove * MoveSpeed * Time.deltaTime);
         
 
         if (rotatedMove != Vector3.zero)
         {
-            Vector3 newDir = Vector3.RotateTowards(transform.forward, rotatedMove, turnSpeed * Time.deltaTime, 0.0f);
+            Vector3 newDir = Vector3.RotateTowards(transform.forward, rotatedMove, TurnSpeed * Time.deltaTime, 0.0f);
     
             // Move our position a step closer to the target
             transform.rotation = Quaternion.LookRotation(newDir);
+        }
+        _isGrounded = Physics.CheckSphere(_groundChecker.position, GroundDistance, _ground, QueryTriggerInteraction.Ignore);
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = 0f;
+        }
+        else
+        {
+            _velocity.y += Physics.gravity.y * Time.deltaTime;
+            _controller.Move(_velocity * Time.deltaTime);
         }
     }
 }
